562b13dff843b1081ddb3b3835206891f960d5f2
HuyAio <99306951+HuyAio@users.noreply.github.com>
2022-02-09 14:59:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: " LỜI MUỐN NÓI",
-    introTitle: 'BẠN GÌ ĐÓ ƠIIIII',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
+    introTitle: 'CẬU GÌ ĐÓ ƠIIIII💖 CẬU đó!!',
+    introDesc: `Trái đất vốn lạ thường😘,
+    Mà sao cậu cứ đi nhầm đường.
     Lạc vào tim tớ lẻ loi,
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^BẮN TIM Ở ĐÂY NÈ^^',
+    Đằng sau chữ yêu đây là thương💟`,
+    btnIntro: '👉BẮN TIM Ở ĐÂY NÈ💘',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy😍 ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tớ <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Nhắn tin bày tỏ đi nè💖💖 .',
+    question: 'Uả Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tớ <3',
+    btnReply: 'Gửi cho CẬU <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33',
+    mess: 'Mình biết mà 🥰. Yêu CẬU nhiều nhiều 😘😘',
+    messDesc: 'Nhắn tin bày tỏ với tớ đi nè💖💖 .',
     btnAccept: 'Okiiiii lun <3',
     messLink: "https://fb.com/suppotr.10"
 }
